77398d34921dd3352e3cee08d7cf583fc1198699
GustavLevin <gustav_levin@hotmail.com>
2023-12-07 16:47:16 +0100

    a

--- main.js
+++ main.js
@@ -42,10 +42,56 @@ if(q3Answer && q3Answer.value === 'true') {
 
 const q4Answer = document.querySelectorAll('input[name="q4"]:checked');
 if(q4Answer.length === 2 &&
-    Array.from(q4Answer).every((answer) => ['Hemse', 'Alternativ 2'].includes(answer.value))
+    Array.from(q4Answer).every((answer) => ['hemse', 'sproge'].includes(answer.value))
 ) {
     correctAnswers++;
 }
+
+// Fråga 5 
+
+const q5Answer = document.querySelector('input[name="q5"]:checked');
+if(q5Answer && q5Answer.value === 'Igelkotte') {
+    correctAnswers++;
+
+}
+
+
+
+// Fråga 6
+
+const q6Answer = document.querySelector('input[name="q6"]:checked');
+if(q6Answer && q6Answer.value === 'false') {
+    correctAnswers++;
+
+}
+
+
+// Fråga 7 
+
+const q7Answer = document.querySelectorAll('input[name="q7"]:checked');
+if(q7Answer.length === 3 &&
+    Array.from(q4Answer).every((answer) => ['gotska', 'karlsö', 'fårö'].includes(answer.value))
+) {
+    correctAnswers++;
+}
+
+// Fråga 8 
+
+const q8Answer = document.querySelectorAll('input[name="q8"]:checked');
+if(q8Answer && q8Answer.value === 'v.32') {
+    correctAnswers++;
+
+}
+
+// Fråga 9 
+
+const q9Answer = document.querySelectorAll('input[name="q9"]:checked');
+
+// Fråga 10
+
+const q10Answer = document.querySelectorAll('input[name="q10"]:checked');
+
+
 
 
 const percentage = (correctAnswers / totalQuestions) * 100;
