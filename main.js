8e68044d82231b510a2c7f744d831bf7f3de1687
GustavLevin <gustav_levin@hotmail.com>
2023-12-11 15:07:31 +0100

    added correct & incorrect for the user and fixes

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const darkModeBtn = document.getElementById('darkModeBtn');
 
 let correctAnswers = 0;
 const totalQuestions = 10;
-
+let userAnswers = []; // Declare userAnswers outside the function
 
 let calculateResults = () => {
 
@@ -22,6 +22,9 @@ let calculateResults = () => {
 let q1Answer = document.querySelector('input[name="q1"]:checked');
 if(q1Answer && q1Answer.value === '10000') {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 
 }
 
@@ -29,7 +32,9 @@ if(q1Answer && q1Answer.value === '10000') {
 let q2Answer = document.querySelector('input[name="q2"]:checked');
 if(q2Answer && q2Answer.value === 'true') {
     correctAnswers++;
-
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 // Fråga 3
@@ -37,7 +42,9 @@ if(q2Answer && q2Answer.value === 'true') {
 let q3Answer = document.querySelector('input[name="q3"]:checked');
 if(q3Answer && q3Answer.value === 'true') {
     correctAnswers++;
-
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 // Fråga 4 check
@@ -47,6 +54,9 @@ if(q4Answer.length === 3 &&
     Array.from(q4Answer).every((answer) => ['hemse', 'sproge','lärbro' ].includes(answer.value))
 ) {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 // Fråga 5 
@@ -54,7 +64,9 @@ if(q4Answer.length === 3 &&
 let q5Answer = document.querySelector('input[name="q5"]:checked');
 if(q5Answer && q5Answer.value === 'Igelkotte') {
     correctAnswers++;
-
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 
@@ -64,6 +76,9 @@ if(q5Answer && q5Answer.value === 'Igelkotte') {
 let q6Answer = document.querySelector('input[name="q6"]:checked');
 if(q6Answer && q6Answer.value === 'false') {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 
 }
 
@@ -72,18 +87,24 @@ if(q6Answer && q6Answer.value === 'false') {
 
 let q7Answer = document.querySelectorAll('input[name="q7"]:checked');
 if(q7Answer.length === 3 &&
-    Array.from(q4Answer).every((answer) => ['gotska', 'karlsö', 'fårö'].includes(answer.value))
+    Array.from(q7Answer).every((answer) => ['gotska', 'karlsö', 'fårö'].includes(answer.value))
 ) {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
+
 
 // Fråga 8 
-
 let q8Answer = document.querySelectorAll('input[name="q8"]:checked');
-if(q8Answer && q8Answer.value === 'Igelkotte') {
+if (q8Answer.length === 1 && q8Answer[0].value === 'v.32') {
     correctAnswers++;
-
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
+
 
 // Fråga 9 
 
@@ -92,6 +113,9 @@ if(q9Answer.length === 3 &&
     Array.from(q9Answer).every((answer) => ['holmhällar', 'sandviken','smöjen' ].includes(answer.value))
 ) {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 
@@ -100,18 +124,24 @@ if(q9Answer.length === 3 &&
 let q10Answer = document.querySelectorAll('input[name="q10"]:checked');
 if(q10Answer && q10Answer.value === '176') {
     correctAnswers++;
+    userAnswers.push(true); // Update userAnswers array
+    } else {
+        userAnswers.push(false); // Update userAnswers array
 }
 
 
 
 const percentage = (correctAnswers / totalQuestions) * 100;
-return percentage;
+return { percentage, userAnswers }; // Return an object with percentage and userAnswers
+
+
 
 }
 
 let showResultBtn = document.getElementById("resultBtn");
 let resultList = document.getElementById("result");
 let popUp = document.querySelector(".popUp");
+let tryAgainBtn;
 
 resultList.innerHTML = "";
 
@@ -119,7 +149,7 @@ showResultBtn.addEventListener("click", () => {
 
     popUp.classList.add("popUpShow");
 
-    const percentage = calculateResults();
+    const { percentage, userAnswers } = calculateResults();
 
     
   resultList.innerText = `Antal rätt: ${correctAnswers} av ${totalQuestions}`;
@@ -127,29 +157,72 @@ showResultBtn.addEventListener("click", () => {
   
   if (percentage < 50) {
     resultList.style.color = "red";
-    resultList.innerText += "Underkänd"; 
+    resultList.innerText += " Underkänd"; 
   } else if (percentage >= 50 && percentage < 75) {
     resultList.style.color = "yellow";
-    resultList.innerText += "Godkänd"; 
+    resultList.innerText += " Godkänd"; 
 
   } else {
     resultList.style.color = "green";
-    resultList.innerText += "Riktigt bra jobbat"; 
+    resultList.innerText += " Riktigt bra jobbat"; 
 
   }
+
+  tryAgainBtn = document.createElement('button');
+    tryAgainBtn.innerText = 'Try Again';
+    tryAgainBtn.id = 'tryAgainBtn';
+    tryAgainBtn.addEventListener('click', tryAgain);
+    popUp.appendChild(tryAgainBtn);
+
   
 
 });
 
+function tryAgain() {
+    // Remove feedback and reset answers
+    popUp.classList.remove("popUpShow");
+    resultList.innerText = "";
+    resultList.style.color = "inherit";
+    userAnswers = [];
+
+    // Remove correct/incorrect classes from question divs
+    document.querySelectorAll('.question').forEach((question) => {
+        question.classList.remove('correct', 'incorrect');
+    });
+
+    // Remove "Try Again" button
+    if (tryAgainBtn) {
+        tryAgainBtn.remove();
+    }
+}
 
 
-
-let answerBtn = document.querySelector("#correctAnswer");
+let answerBtn = document.getElementById("correctAnswer");
 
 answerBtn.addEventListener("click", () => {
+    const { userAnswers } = calculateResults();
+
+    // Iterate through each question
+    userAnswers.forEach((answer, index) => {
+        let questionDivs = document.querySelectorAll('.question');
+        let questionDiv = questionDivs[index];
+
+        if (questionDiv) {
+            let isCorrect = document.createElement("h3");
+
+            if (answer === true) {
+                questionDiv.classList.add('correct');
+                isCorrect.innerText = "Korrekt svar";
+            } else {
+                questionDiv.classList.add('incorrect');
+                isCorrect.innerText = "Felaktigt svar";
+            }
+
+            // Append isCorrect to the questionDiv
+            questionDiv.appendChild(isCorrect);
+        }
+    });
+
     popUp.classList.remove("popUpShow");
-
-
-
-
 });
+
